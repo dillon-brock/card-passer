c3619d96ff1d1f9e887532065715ed9661d464db
Dillon Brock <mdillon1297@gmail.com>
2022-10-03 11:16:50 -0700

    moved state and functions findCardIndex and passCard into GameContext.js. Have yet to refactor components to use context

--- src/App.js
+++ src/App.js
@@ -6,39 +6,39 @@ import { useState } from 'react';
 import ExecutePassButton from './components/ExecutePassButton';
 
 function App() {
-  const [deck, setDeck] = useState(initialCards);
-  const [playerOneHand, setPlayerOneHand] = useState([]);
-  const [selectedCard, setSelectedCard] = useState();
-  const [playerTwoHand, setPlayerTwoHand] = useState([]);
-  const [playerThreeHand, setPlayerThreeHand] = useState([]);
-  const [from, setFrom] = useState('deck');
-  const [to, setTo] = useState(1);
+  // const [deck, setDeck] = useState(initialCards);
+  // const [playerOneHand, setPlayerOneHand] = useState([]);
+  // const [selectedCard, setSelectedCard] = useState();
+  // const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  // const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  // const [from, setFrom] = useState('deck');
+  // const [to, setTo] = useState(1);
 
-  function findCardIndex(value, suit, cards) {
-    return cards.findIndex((card) => card.value === value && card.suit === suit);
-  }
+  // function findCardIndex(value, suit, cards) {
+  //   return cards.findIndex((card) => card.value === value && card.suit === suit);
+  // }
 
-  function passCard(card) {
-    const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
-    const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
+  // function passCard(card) {
+  //   const playerHands = [playerOneHand, playerTwoHand, playerThreeHand];
+  //   const playerHandSetFunctions = [setPlayerOneHand, setPlayerTwoHand, setPlayerThreeHand];
 
-    // arrays start at zero, but our players start at 1 :shrug:
-    const toHand = playerHands[to - 1] || deck;
-    const fromHand = playerHands[from - 1] || deck;
+  //   // arrays start at zero, but our players start at 1 :shrug:
+  //   const toHand = playerHands[to - 1] || deck;
+  //   const fromHand = playerHands[from - 1] || deck;
 
-    const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
-    const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
+  //   const toSetFunction = playerHandSetFunctions[to - 1] || setDeck;
+  //   const fromSetFunction = playerHandSetFunctions[from - 1] || setDeck;
 
-    const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
-    const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
+  //   const cardToMoveIndex = findCardIndex(card.value, card.suit, fromHand);
+  //   const [cardToMove] = fromHand.splice(cardToMoveIndex, 1);
 
-    toHand.push(cardToMove);
+  //   toHand.push(cardToMove);
 
-    toSetFunction([...toHand]);
-    fromSetFunction([...fromHand]);
+  //   toSetFunction([...toHand]);
+  //   fromSetFunction([...fromHand]);
 
-    setSelectedCard(null);
-  }
+  //   setSelectedCard(null);
+  // }
 
   return (
     <div className="App">
